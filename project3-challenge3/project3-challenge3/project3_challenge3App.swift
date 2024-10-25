65ae0d9dd689a1f98c16af4ee3e554708bedae44
Ali Syed <alisyed0206@gmail.com>
2024-10-25 12:31:25 -0400

    start files for project 3, challenge 3

---
+++ project3-challenge3/project3-challenge3/project3_challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  project3_challenge3App.swift
+//  project3-challenge3
+//
+//  Created by Ali Syed on 2024-10-25.
+//
+
+import SwiftUI
+
+@main
+struct project3_challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ project3-challenge3/project3-challenge3Tests/project3_challenge3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  project3_challenge3Tests.swift
+//  project3-challenge3Tests
+//
+//  Created by Ali Syed on 2024-10-25.
+//
+
+import Testing
+@testable import project3_challenge3
+
+struct project3_challenge3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
